dd3b1f4c1b87138922449811043fdeb0e5f8ebe0
Patrick Mauel <patrickmauel@Patricks-MBP-2.mediacom.info>
2020-06-08 16:42:28 -0500

    Updates

---
+++ precache-manifest.f60a8c24221098842455ac2aeeb57780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ae4830633432b6640502318f53c692",
+    "url": "/bisviewer/index.html"
+  },
+  {
+    "revision": "5c30b841aae987ccf7b7",
+    "url": "/bisviewer/static/css/main.b0a92eb1.chunk.css"
+  },
+  {
+    "revision": "dbe088100f136ee7ed6b",
+    "url": "/bisviewer/static/js/2.9da8d80a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bisviewer/static/js/2.9da8d80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c30b841aae987ccf7b7",
+    "url": "/bisviewer/static/js/main.70164e0a.chunk.js"
+  },
+  {
+    "revision": "34f212ffd3ba76a3a340",
+    "url": "/bisviewer/static/js/runtime-main.b11139f4.js"
+  }
+]);
